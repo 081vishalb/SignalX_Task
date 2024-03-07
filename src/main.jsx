d70fdeead9c57a61f6a1053014d4fc85476f9400
Vishal <bvishal081@gmail.com>
2024-03-07 16:11:06 +0530

    this is second publish

--- src/main.jsx
+++ src/main.jsx
@@ -26,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       {/* <App /> */}
+      
     </QueryClientProvider>
   </React.StrictMode>,
 )
